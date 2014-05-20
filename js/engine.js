888cd122cce3505eca4a9846954f4da38fdd7e5a
Gaby Rock <gabyrock@hotmail.co.uk>
2014-05-20 11:43:27 +0100

    initial commit with added levels

---
+++ js/engine.js
@@ -0,0 +1,383 @@
+var Game = new function() {
+
+    // KEYBOARD CONTROL - ADDING NEW KEYS - JAVASCRIPT KEYCODES - LINK IN GAME.JS
+
+  var KEY_CODES = { 37:'left', 39:'right', 32 :'fire', 38:'up', 40:'down'};
+  this.keys = {};
+
+  // INITIALIZE CANVAS
+
+  this.initialize = function(canvas_dom,level_data,sprite_data,callbacks) {
+    this.canvas_elem = $(canvas_dom)[0];
+    this.canvas = this.canvas_elem.getContext('2d');
+    this.width = $(this.canvas_elem).attr('width');
+    this.height= $(this.canvas_elem).attr('height');
+
+    // SET UP KEY BINDINGS
+    
+    $(window).keydown(function(event) {
+      if(KEY_CODES[event.keyCode]) Game.keys[KEY_CODES[event.keyCode]] = true;
+    });
+
+    $(window).keyup(function(event) {
+      if(KEY_CODES[event.keyCode]) Game.keys[KEY_CODES[event.keyCode]] = false;
+    });
+
+    this.level_data = level_data;
+    this.callbacks = callbacks;
+    Sprites.load(sprite_data,this.callbacks['start']);
+  };
+
+  // RUN THE GAME LOOP
+
+  this.loadBoard = function(board) { Game.board = board; };
+
+  // RENDER TO SCREEN
+
+  this.loop = function() { 
+    Game.board.step(30/1000); 
+    Game.board.render(Game.canvas);
+    setTimeout(Game.loop,30);
+  };
+};
+
+// lOAD RESOURCES
+
+var Sprites = new function() {
+  this.map = { }; 
+
+  this.load = function(sprite_data,callback) { 
+    this.map = sprite_data;
+    this.image = new Image();
+    this.image.onload = callback;
+      
+    //WHERE YOU CHANGE IMAGE OF SPRITES
+
+    this.image.src = 'images/sprites.png';
+  };
+
+  this.draw = function(canvas,sprite,x,y,frame) {
+    var s = this.map[sprite];
+    if(!frame) frame = 0;
+    canvas.drawImage(this.image, s.sx + frame * s.w, s.sy, s.w, s.h, x,y, s.w, s.h);
+  };
+}
+
+// THE LOADING SCREEN
+
+var GameScreen = function GameScreen(text,text2,callback) {
+  this.step = function(dt) {
+    if(Game.keys['fire'] && callback) callback();
+  };
+
+  this.render = function(canvas) {
+    canvas.clearRect(0,0,Game.width,Game.height);
+    canvas.font = "bold 40px arial";
+    var measure = canvas.measureText(text);  
+    canvas.fillStyle = "#FFFFFF";
+    canvas.fillText(text,Game.width/2 - measure.width/2,Game.height/2);
+    canvas.font = "bold 20px arial";
+    var measure2 = canvas.measureText(text2);
+    canvas.fillText(text2,Game.width/2 - measure2.width/2,Game.height/2 + 40);
+  };
+};
+
+//ADD OR REMOVE SPRITES
+
+var GameBoard = function GameBoard(level_number) {
+  this.removed_objs = [];
+  this.missiles = 0;
+  this.level = level_number;
+  var board = this;
+
+  this.add = function(obj) { obj.board=this; this.objects.push(obj); return obj; };
+  this.remove = function(obj) { this.removed_objs.push(obj); };
+
+  this.addSprite = function(name,x,y,opts) {
+    var sprite = this.add(new Sprites.map[name].cls(opts));
+    sprite.name = name;
+    sprite.x = x; sprite.y = y;
+    sprite.w = Sprites.map[name].w; 
+    sprite.h = Sprites.map[name].h;
+    return sprite;
+  };
+  
+
+  this.iterate = function(func) {
+     for(var i=0,len=this.objects.length;i<len;i++) {
+       func.call(this.objects[i]);
+     }
+  };
+
+  this.detect = function(func) {
+    for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
+      if(func.call(this.objects[i])) return this.objects[i];
+    }
+    return false;
+  };
+
+  //STEP AND RENDER
+
+  this.step = function(dt) { 
+    this.removed_objs = [];
+    this.iterate(function() { 
+        if(!this.step(dt)) this.die();
+    }); 
+
+    for(var i=0,len=this.removed_objs.length;i<len;i++) {
+      var idx = this.objects.indexOf(this.removed_objs[i]);
+      if(idx != -1) this.objects.splice(idx,1);
+    }
+  };
+
+  this.render = function(canvas) {
+    canvas.clearRect(0,0,Game.width,Game.height);
+    this.iterate(function() { this.draw(canvas); });
+  };
+
+  //DETECTING COLLISIONS
+
+  this.collision = function(o1,o2) {
+    return !((o1.y+o1.h-1<o2.y) || (o1.y>o2.y+o2.h-1) ||
+             (o1.x+o1.w-1<o2.x) || (o1.x>o2.x+o2.w-1));
+  };
+
+  this.collide = function(obj) {
+    return this.detect(function() {
+      if(obj != this && !this.invulnrable)
+       return board.collision(obj,this) ? this : false;
+    });
+  };
+
+  //LOADING A LEVEL
+
+  this.loadLevel = function(level) {
+    this.objects = [];
+    this.player = this.addSprite('player', // Sprite
+                                 Game.width/2, // X
+                                 Game.height - Sprites.map['player'].h - 10); // Y
+
+    // LEVEL_DATA BECOMES "LEVEL"
+
+    var flock = this.add(new AlienFlock());
+    for(var y=0,rows=level.length;y<rows;y++) {
+      for(var x=0,cols=level[y].length;x<cols;x++) {
+        var alien = Sprites.map['alien' + level[y][x]];
+        if(alien) { 
+          this.addSprite('alien' + level[y][x], // Which Sprite
+                         (alien.w+10)*x,  // X
+                         alien.h*y,       // Y
+                         { flock: flock }); // Options
+        }
+      }
+    }
+  };
+
+  this.nextLevel = function() { 
+    return Game.level_data[level_number + 1] ? (level_number + 1) : false 
+  };
+ 
+  this.loadLevel(Game.level_data[level_number]);
+};
+
+//AUDIO CHANNELS
+
+var GameAudio = new function() {
+  this.load_queue = [];
+  this.loading_sounds = 0;
+  this.sounds = {};
+
+  var channel_max = 10;		
+  audio_channels = new Array();
+  for (a=0;a<channel_max;a++) {	
+    audio_channels[a] = new Array();
+    audio_channels[a]['channel'] = new Audio(); 
+    audio_channels[a]['finished'] = -1;	
+  }
+
+  //LOADING THE AUDIO
+
+  this.load = function(files,callback) {
+    var audioCallback = function() { GameAudio.finished(callback); }
+
+    for(name in files) {
+      var filename = files[name];
+      this.loading_sounds++;
+      var snd = new Audio();
+      this.sounds[name] = snd;
+      snd.addEventListener('canplaythrough',audioCallback,false);
+      snd.src = filename;
+      snd.load();
+    }
+  };
+
+  this.finished = function(callback) {
+    this.loading_sounds--;
+    if(this.loading_sounds == 0) {
+      callback();
+    }
+  };
+
+  //PLAYING A SOUND
+
+  this.play = function(s) {
+    for (a=0;a<audio_channels.length;a++) {
+      thistime = new Date();
+      if (audio_channels[a]['finished'] < thistime.getTime()) {	
+        audio_channels[a]['finished'] = thistime.getTime() + this.sounds[s].duration*1000;
+        audio_channels[a]['channel'].src = this.sounds[s].src;
+        audio_channels[a]['channel'].load();
+        audio_channels[a]['channel'].play();
+        break;
+      }
+    }
+  };
+};
+
+var game = new Game();
+
+function init() {
+  if(game.init())
+    game.start();
+}
+
+/**
+ * Define an object to hold all our images for the game so images
+ * are only ever created once. This type of object is known as a 
+ * singleton.
+ */
+var imageRepository = new function() {
+  // Define images
+  this.empty = null;
+  this.background = new Image();
+  
+  // Set images src
+  this.background.src = "imgs/bg.png";
+}
+
+
+/**
+ * Creates the Drawable object which will be the base class for
+ * all drawable objects in the game. Sets up defualt variables
+ * that all child objects will inherit, as well as the defualt
+ * functions. 
+ */
+function Drawable() { 
+  
+  /** 
+  allows us to set the x and y position of the object created 
+  *It also defines the speed of the object 
+  *(or how many pixels the object can move each frame)
+  *the width and height of the object’s containing canvas. 
+  */
+  this.init = function(x, y) {
+    // Defualt variables
+    this.x = x;
+    this.y = y;
+  }
+
+  this.speed = 0;
+  this.canvasWidth = 0;
+  this.canvasHeight = 0;
+  
+  // Define abstract function to be implemented in child objects
+  this.draw = function() {
+  };
+}
+
+
+/**
+ * Creates the Background object which will become a child of
+ * the Drawable object. The background is drawn on the "background"
+ * canvas and creates the illusion of moving by panning the image.
+ */
+function Background() {
+  this.speed = 1; // Redefine speed of the background for panning
+  
+  // Implement abstract function
+  this.draw = function() {
+    // Pan background
+    this.y += this.speed;
+    this.context.drawImage(imageRepository.background, this.x, this.y);
+    
+    // Draw another image at the top edge of the first image
+    this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight);
+
+    // If the image scrolled off the screen, reset
+    if (this.y >= this.canvasHeight)
+      this.y = 0;
+  };
+}
+// Set Background to inherit properties from Drawable
+Background.prototype = new Drawable();
+
+
+/**
+ * Creates the Game object which will hold all objects and data for
+ * the game.
+ */
+function Game() {
+  /*
+   * Gets canvas information and context and sets up all game
+   * objects. 
+   * Returns true if the canvas is supported and false if it
+   * is not. This is to stop the animation script from constantly
+   * running on older browsers.
+   */
+  this.init = function() {
+    // Get the canvas element
+    this.bgCanvas = document.getElementById('background');
+    
+    // Test to see if canvas is supported
+    if (this.bgCanvas.getContext) {
+      this.bgContext = this.bgCanvas.getContext('2d');
+    
+      // Initialize objects to contain their context and canvas
+      // information
+      Background.prototype.context = this.bgContext;
+      Background.prototype.canvasWidth = this.bgCanvas.width;
+      Background.prototype.canvasHeight = this.bgCanvas.height;
+      
+      // Initialize the background object
+      this.background = new Background();
+      this.background.init(0,0); // Set draw point to 0,0
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  // Start the animation loop
+  this.start = function() {
+    animate();
+  };
+}
+
+
+/**
+ * The animation loop. Calls the requestAnimationFrame shim to
+ * optimize the game loop and draws all game objects. This
+ * function must be a gobal function and cannot be within an
+ * object.
+ */
+function animate() {
+  requestAnimFrame( animate );
+  game.background.draw();
+}
+
+
+/** 
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop, 
+ * otherwise defaults to setTimeout().
+ */
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+      window.webkitRequestAnimationFrame || 
+      window.mozRequestAnimationFrame    || 
+      window.oRequestAnimationFrame      || 
+      window.msRequestAnimationFrame     || 
+      function(/* function */ callback, /* DOMElement */ element){
+        window.setTimeout(callback, 1000 / 60);
+      };
+})();
---
+++ js/game.js
@@ -0,0 +1,167 @@
+
+//SPEED OF ALIENS - THE ALIEN FLOCK
+
+var AlienFlock = function AlienFlock() {
+  this.invulnrable = true;
+  this.dx = 10; this.dy = 0;
+  this.hit = 1; this.lastHit = 0;
+  this.speed = 10;
+
+  this.draw = function() {};
+
+  this.die = function() {
+    if(Game.board.nextLevel()) {
+      Game.loadBoard(new GameBoard(Game.board.nextLevel())); 
+    } else {
+      Game.callbacks['win']();
+    }
+  }
+
+  this.step = function(dt) { 
+    if(this.hit && this.hit != this.lastHit) {
+      this.lastHit = this.hit;
+      this.dy = this.speed;
+    } else {
+      this.dy=0;
+    }
+    this.dx = this.speed * this.hit;
+
+    var max = {}, cnt = 0;
+    this.board.iterate(function() {
+      if(this instanceof Alien)  {
+        if(!max[this.x] || this.y > max[this.x]) {
+          max[this.x] = this.y; 
+        }
+        cnt++;
+      } 
+    });
+
+    if(cnt == 0) { this.die(); } 
+
+    this.max_y = max;
+    return true;
+  };
+
+}
+
+//ALIEN FUNCTIONS
+
+var Alien = function Alien(opts) {
+  this.flock = opts['flock'];
+  this.frame = 0;
+  this.mx = 0;
+}
+
+// THIS IS THE LOGIC FOR THE ALIENS STEPS + MOVEMENTS
+
+Alien.prototype.draw = function(canvas) {  Sprites.draw(canvas,this.name,this.x,this.y,this.frame);
+}
+
+//THE SPEED INCREMENTS WHEN THE ALIEN DIES
+
+Alien.prototype.die = function() {
+  GameAudio.play('die');
+//THE SPEED EACH TIME YOU KILL AN ALIEN
+  this.flock.speed += 2;
+  this.board.remove(this);
+}
+
+Alien.prototype.step = function(dt) {
+  this.mx += dt * this.flock.dx;
+  this.y += this.flock.dy;
+ // THE SPEED MOVEMENTS OF ATTACKER ALIENS
+  if(Math.abs(this.mx) > 10) {
+    if(this.y == this.flock.max_y[this.x]) {
+      this.fireSometimes();
+    }
+    this.x += this.mx;
+    this.mx = 0;
+    // THIS CHANGES / ADDS FRAMES FOR SPRITE SHEET %2
+    this.frame = (this.frame+1) % 2;
+    if(this.x > Game.width - Sprites.map.alien1.w * 2) this.flock.hit = -1;
+    if(this.x < Sprites.map.alien1.w) this.flock.hit = 1;
+  }
+  return true;
+}
+
+// THE ALIENS SHOOTING FIRE
+
+Alien.prototype.fireSometimes = function() {
+    // LOGIC TO SET FREQUENCY OF ALIEN FIRE - IF NUMBER IS LESS THAN 100 - SHOOT MISSILE
+      if(Math.random()*100 < 10) {
+        this.board.addSprite('missile',this.x + this.w/2 - Sprites.map.missile.w/2,
+                                      this.y + this.h, 
+                                     { dy: 100 });
+      }
+}
+
+//THE PLAYER
+
+var Player = function Player(opts) { 
+  this.reloading = 0;
+}
+
+Player.prototype.draw = function(canvas) {
+   Sprites.draw(canvas,'player',this.x,this.y);
+}
+
+
+Player.prototype.die = function() {
+  GameAudio.play('die');
+  Game.callbacks['die']();
+}
+
+// CONTROL MOVING ALONG THE AXIS - LEFT/RIGHT/UP/DOWN / THE SPEED
+Player.prototype.step = function(dt) {
+  if(Game.keys['left']) { this.x -= 100 * dt; }
+  if(Game.keys['right']) { this.x += 100 * dt; }
+  if(Game.keys['up']) { this.y -= 100 * dt; }
+  if(Game.keys['down']) { this.y += 100* dt;}
+
+
+  if(this.x < 0) this.x = 0;
+  if(this.x > Game.width-this.w) this.x = Game.width-this.w;
+
+  this.reloading--;
+// CHANGES THE NUMBER + TYPES OF MISSILES < 10 
+  if(Game.keys['fire'] && this.reloading <= 0 && this.board.missiles < 10) {
+    GameAudio.play('fire');
+    this.board.addSprite('missile',
+                          this.x + this.w/2 - Sprites.map.missile.w/2,
+                          this.y-this.h,
+                          { dy: -100, player: true });
+    this.board.missiles++;
+      // THE TIME BETWEEN SHOOTING THE MISSILES
+    this.reloading = 10;
+  }
+  return true;
+}
+
+
+var Missile = function Missile(opts) {
+   this.dy = opts.dy;
+   this.player = opts.player;
+}
+
+// THE MISSILES
+
+Missile.prototype.draw = function(canvas) {
+   Sprites.draw(canvas,'missile',this.x,this.y);
+}
+
+Missile.prototype.step = function(dt) {
+   this.y += this.dy * dt;
+
+   var enemy = this.board.collide(this);
+   if(enemy) { 
+     enemy.die();
+     return false;
+   }
+   return (this.y < 0 || this.y > Game.height) ? false : true;
+}
+
+Missile.prototype.die = function() {
+  if(this.player) this.board.missiles--;
+  if(this.board.missiles < 0) this.board.missiles=0;
+   this.board.remove(this);
+}
